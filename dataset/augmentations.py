04d8a21b825359dc9c4aeb29a6c382acfc588105
Giovanni Cavallin <37183651+mawanda-jun@users.noreply.github.com>
2021-11-04 10:39:04 +0100

    now dataset is working also with bigger images

--- dataset/augmentations.py
+++ dataset/augmentations.py
@@ -4,9 +4,12 @@ import albumentations as A
 def get_augmentations():
     return A.Compose(
         [
+            # A.CropNonEmptyMaskIfExists(),
             A.RandomCrop(400, 400, always_apply=True),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
             # A.RandomContrast(),
         ],
-        bbox_params=A.BboxParams(format='pascal_voc', label_fields=['bbox_classes'])
+        bbox_params=A.BboxParams(format='pascal_voc', label_fields=['bbox_classes'], min_area=480)
     )
 
--- dataset/cell_dataset.py
+++ dataset/cell_dataset.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset, DataLoader
 from PIL import Image
 import os
 import numpy as np
-from cell_utils import analyze_sample
+from cell_utils import analyze_sample, collate_fn
 from pycocotools.coco import COCO
 from augmentations import get_augmentations
 
@@ -20,16 +20,19 @@ class CellDataset(Dataset):
         return len(self.img_ids)
 
     def __getitem__(self, idx):
-        # load images ad masks
+        # Use in case of searching specific image with string ID
+        if isinstance(idx, str):
+            idx = self.img_ids.index(idx)
+
         img_info = self.coco.loadImgs([self.img_ids[idx]])[0]
 
         img = np.array(Image.open(os.path.join(self.data_dir, img_info['file_name'])))
+        # img = np.array([0])
         anns_ids = self.coco.getAnnIds(imgIds=[img_info['id']])
         anns = self.coco.loadAnns(anns_ids)
         # Create empty boxes list
         boxes = []
-        # Function to transform coco labels into mask-rcnn ones
-        coco_to_mrcnn = lambda x_min, y_min, width, height: [x_min, y_min, x_min+width, y_min+height]
+
         # Create empty label list
         labels = []
         # Create is_crowd list (dummy, they are all 0)
@@ -38,13 +41,14 @@ class CellDataset(Dataset):
         areas = []
         # Create empty mask object
         masks = []
+        # Function to transform coco labels into mask-rcnn ones
+        coco_to_mrcnn = lambda x_min, y_min, width, height: [x_min, y_min, x_min + width, y_min + height]
         for ann in anns:
             masks.append(self.coco.annToMask(ann))
             boxes.append(coco_to_mrcnn(*ann['bbox']))
-            labels.append(ann['category_id'] - 1)  # labels are 1, 2, 3 so we shift them to 0
+            labels.append(ann['category_id'])  # labels are 1, 2, 3
             areas.append(ann['area'])
             iscrowd.append(ann['iscrowd'])
-
 
         if self.transforms is not None:
             transformed = self.transforms(
@@ -55,7 +59,7 @@ class CellDataset(Dataset):
             )
             img = transformed['image']
             masks = transformed['masks']
-            boxes = [[round(el) for el in box] for box in transformed['bboxes'] ]
+            boxes = [[round(el) for el in box] for box in transformed['bboxes']]
 
         # Required target for the Mask R-CNN
         target = {
@@ -71,9 +75,35 @@ class CellDataset(Dataset):
 
 
 if __name__ == '__main__':
+    from tqdm import tqdm
+    import matplotlib.pyplot as plt
     trans = get_augmentations()
-    dataset = CellDataset('../data', '../data/annotations_train_4X.json', transforms=trans)
-    dataloader = DataLoader(dataset)
-    for img, target in dataset:
-        analyze_sample(img, target)
-        break
+    dataset = CellDataset('../data', '../data/train_4X.json', transforms=trans)
+    dataloader = DataLoader(
+        dataset,
+        num_workers=0,
+        collate_fn=collate_fn
+    )
+    img, target = dataset['52f65c9194c0']
+    analyze_sample(img, target)
+    # box_areas = []
+    # min_bounding_box = 10000000000
+    # max_bounding_box = 0
+    # max_id = ''
+    # for batch in tqdm(dataloader):
+    #     imgs, targets = batch
+    #     for img, target in zip(imgs, targets):
+    #         for area in target['area']:
+    #             box_areas.append(area)
+    #             if area < min_bounding_box:
+    #                 min_bounding_box = area
+    #             if area > max_bounding_box:
+    #                 max_bounding_box = area
+    #                 max_id = target['image_id']
+    #         analyze_sample(img, target)
+    #         # break
+    # print(f"ID of image with bigger bbox: {max_id}")
+    # # print(box_areas)
+    # print(f"Smallest bbx: {min_bounding_box}")
+    # pass
+    #
