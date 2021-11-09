de50f285337f55ee3ba7c6d75c4c6174c6b4c2b1
mawanda-jun <giovanni.cavallin@outlook.com>
2021-11-09 11:12:15 +0100

    fix augmentations

--- dataset/augmentations.py
+++ dataset/augmentations.py
@@ -4,18 +4,15 @@ import albumentations as A
 def get_augmentations():
     return A.Compose(
         [
-            # A.CropNonEmptyMaskIfExists(),
-            # A.Crop(25, 25, 64, 64, always_apply=True),
-            # A.Crop(400, 400, always_apply=True),
-            # A.RandomCrop(400, 400, always_apply=True),
-            # A.HorizontalFlip(p=0.5),
-            # A.VerticalFlip(p=0.5),
+            A.RandomCrop(520, 704, always_apply=True),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.5),
             # A.RandomContrast(),
         ],
         bbox_params=A.BboxParams(
             format='pascal_voc',
             label_fields=['bbox_classes'],
-            # min_area=20
+            min_area=480
         )
     )
 
